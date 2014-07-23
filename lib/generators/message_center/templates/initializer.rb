bc804eda6b5ea2a9c7f8c43a9fe94eb4a73b1a86
Dave Gynn <dgynn@unow.com>
2014-07-23 16:34:54 -0700

    add message_class to generated config initializer

--- lib/generators/message_center/templates/initializer.rb
+++ lib/generators/message_center/templates/initializer.rb
@@ -1,14 +1,24 @@
 MessageCenter.setup do |config|
 
+  #Configures the Messagable class
+  # config.messageable_class = 'User'
+
   #Configures if you application uses or not email sending for Notifications and Messages
   config.uses_emails = true
 
+  #Configures whether mailer should be receive deliveries once or as a group
+  # config.mailer_wants_array = false
+
   #Configures the default from for emails sent for Messages and Notifications
-  config.default_from = "no-reply@mailboxer.com"
+  config.default_from = "no-reply@yourdomain.com"
 
   #Configures the methods needed by mailboxer
   config.email_method = :mailboxer_email
   config.name_method = :name
+
+  #Configures custom implementation of notification and message mailers
+  # config.notification_mailer = MessageCenter::NotificationMailer
+  # config.message_mailer = MessageCenter::MessageMailerMailer
 
   #Configures if you use or not a search engine and which one you are using
   #Supported engines: [:solr,:sphinx]
